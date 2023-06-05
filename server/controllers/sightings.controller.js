032a2ea3c288d17f5ac1ce4aa074a9a77dc64e25
Christian Johnson <charcoaldust@mac.com>
2023-06-04 21:02:38 -0700

    Add createSighting function
    This commit adds the createSighting function in the sightings controller to use the Sighting model to create a new sighting. The function now logs the request body and the created sighting to the console, and returns the created sighting in the response.
    
    - Use Sighting model to create new sighting
    - Log request body and created sighting to console
    - Return created sighting in response

--- server/controllers/sightings.controller.js
+++ server/controllers/sightings.controller.js
@@ -3,3 +3,16 @@ module.exports.index = (request, response) => {
     message: "Hello World",
   });
 };
+//*-------------- CREATE --------------*//
+module.exports.createSighting = (request, response) => {
+  console.log("request.body", request.body);
+  Sighting.create(request.body)
+    .then((sighting) => {
+      console.log("sighting", sighting);
+      response.json(sighting);
+    })
+    .catch((err) => {
+      console.log("err", err);
+      response.status(400).json(err);
+    });
+};
