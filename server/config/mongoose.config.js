bb7aa88c39687bd4bb6fe085667b6db2c475aea4
Christian Johnson <charcoaldust@mac.com>
2023-06-04 20:31:18 -0700

    new config for MongoDB

---
+++ server/config/mongoose.config.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const dbName = "sightingsDB";
+mongoose
+  .connect("mongodb://127.0.01:27017/${dbName}", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log(`Established a connection to the database ${dbName}`))
+  .catch((err) => console.log(`Error connecting to ${dbName}`, err));
