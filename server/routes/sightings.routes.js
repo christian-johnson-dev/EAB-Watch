71d5e9cf58f78b6f93d66290203d60d09f7e0926
Christian Johnson <charcoaldust@mac.com>
2023-06-04 21:07:35 -0700

    Add POST route for creating sightings
    This commit adds a new POST route to the sightings router for creating sightings. The route maps to the createSighting function in the sightings controller.
    
    - Add POST route for /api/sightings
    - Map route to createSighting function in controller

--- server/routes/sightings.routes.js
+++ server/routes/sightings.routes.js
@@ -1,4 +1,5 @@
 const SightingsController = require("../controllers/sightings.controller");
 module.exports = (app) => {
   app.get("/api", SightingsController.index);
+  app.post("/api/sightings", SightingsController.createSighting);
 };
