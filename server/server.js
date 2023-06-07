5c233bb109b27f31837469f3ec1114b45c2b8def
Christian Johnson <charcoaldust@mac.com>
2023-06-06 19:02:40 -0700

    Clarified App methods and imports

--- server/server.js
+++ server/server.js
@@ -3,9 +3,11 @@ const app = express();
 const port = 8000;
 const cors = require("cors");
 
+require("./config/mongoose.config");
+require("./routes/sightings.routes")(app);
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.use(cors());
-require("./routes/sightings.routes")(app);
+
 app.use(express.json(), express.urlencoded({ extended: true }));
-require("./config/mongoose.config");
