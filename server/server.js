ed2c82d1e464fa4004aa80e1b8425446c319d403
Christian Johnson <charcoaldust@mac.com>
2023-06-08 13:16:19 -0700

    fix: Rearrange middleware and routes in server.js
    This commit rearranges the code in `server.js` so that middleware is defined before routes. The `cors` and `express.json()` middleware are now defined before the routes in `server.js`.
    
    This change was made to fix an issue where the JSON parser was not being applied to requests before the routes were defined, causing the request body to not be parsed correctly. This could explain why user and admin data were not being seen. By rearranging the middleware and routes, the JSON parser is now applied to requests before the routes are defined, ensuring that the request body is parsed correctly.

--- server/server.js
+++ server/server.js
@@ -3,11 +3,10 @@ const app = express();
 const port = 8000;
 const cors = require("cors");
 
+app.use(cors());
+app.use(express.json(), express.urlencoded({ extended: true }));
+
 require("./config/mongoose.config");
 require("./routes/sightings.routes")(app);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-app.use(cors());
-
-app.use(express.json(), express.urlencoded({ extended: true }));
