2c1d623ef74410e3ac4440f9ad8bd9c754f4b5f6
Christian Johnson <charcoaldust@mac.com>
2023-06-08 10:58:41 -0700

    feat: Add "primary indicators" input to Step8 component
    This commit adds two new checkboxes to the Step8 component for primary indicators of an emerald ash borer infestation: "Serpentine Feeding Galleries" and "D-Shaped Exit Holes". The checkboxes are implemented using the `formData` state and the `handleCheckboxChange` function.

--- client/src/components/wizzardForm/Step8.js
+++ client/src/components/wizzardForm/Step8.js
@@ -1,10 +1,59 @@
 import React from "react";
 
-const Step8 = ({ previous, next }) => {
-  // Component Logic
+const Step8 = ({ formData, handleCheckboxChange, previous, next }) => {
+  const handleLabelClick = (e, name) => {
+    e.preventDefault();
+    handleCheckboxChange({ target: { name, checked: !formData[name] } });
+  };
   return (
     <div>
-      <h2>Step8 reporting for duty</h2>
+      <h2>Primary Indicators</h2>
+
+      <div onClick={(event) => handleLabelClick(event, "feedingGallery")}>
+        <label htmlFor="feedingGallery">
+          <input
+            type="checkbox"
+            name="feedingGallery"
+            checked={formData.feedingGallery}
+            onChange={handleCheckboxChange}
+          />
+          <h3>Serpentine Feeding Galleries</h3>
+          <p>
+            As the larvae eat through the tree, they leave winding paths full of
+            sawdust just under the bark.
+          </p>
+        </label>
+      </div>
+
+      <div onClick={(event) => handleLabelClick(event, "exitHoles")}>
+        <label htmlFor="exitHoles">
+          <input
+            type="checkbox"
+            name="exitHoles"
+            checked={formData.exitHoles}
+            onChange={handleCheckboxChange}
+          />
+          <h3>D-Shaped Exits in Bark</h3>
+          <p>
+            As the larvae mature, they chew their way out of the tree, leaving
+            D-shaped exit holes.
+          </p>
+        </label>
+      </div>
+
+      <div onClick={(event) => handleLabelClick(event, "barkSplitting")}>
+        <label htmlFor="barkSplitting">
+          <input
+            type="checkbox"
+            name="barkSplitting"
+            checked={formData.barkSplitting}
+            onChange={handleCheckboxChange}
+          />
+          <h3>Bark Splitting</h3>
+          <p>Bark splits to reveal S-shaped galleries packed with sawdust.</p>
+        </label>
+      </div>
+
       <button onClick={previous}>Previous</button>
       <button onClick={next}>Next</button>
     </div>
