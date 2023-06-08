607e70b2c559dd19329d9d1f4f29e50d50d08181
Christian Johnson <charcoaldust@mac.com>
2023-06-08 10:54:07 -0700

    feat: Add ash tree question to Step6 component
    This commit adds a new question to the Step6 component asking users if the sighting is on an ash tree. The question is implemented using radio buttons with three options: "Yes" true, "No" false , and "Not Sure" remains undefined. The `handleAshTreeChange` function is used to update the `formData` state with the selected option.

--- client/src/components/wizzardForm/Step6.js
+++ client/src/components/wizzardForm/Step6.js
@@ -1,10 +1,72 @@
 import React from "react";
 
-const Step6 = ({ previous, next }) => {
-  // Component Logic
+const Step6 = ({ setFormData, formData, previous, next }) => {
+  const handleAshTreeChange = (e) => {
+    const value = e.target.value;
+    setFormData({
+      ...formData,
+      isAshTree:
+        value === "true" ? true : value === "false" ? false : undefined,
+    });
+  };
   return (
     <div>
-      <h2>Step6 reporting for duty</h2>
+      <h2>Ash Tree Identification</h2>
+      <p>
+        Emerald Ash Borer's will only infest Ash trees. Below are a couple
+        visual clues to identify an Ash tree.
+      </p>
+      <button>That's OK. I'm sure this is an Ash.</button>
+      <div>
+        Card 1
+        <p>
+          Ash leaves grow with opposing leaflets and a single leaflet at the
+          end.
+        </p>
+      </div>
+      <div>
+        Card 2
+        <p>Ash branches generally grow from the trunk in opposing pairs.</p>
+      </div>
+      <div>
+        Card 3
+        <p>
+          Ash bark is generally smooth in young trees and becomes more
+          diamond-shaped and ridged as the tree matures.
+        </p>
+      </div>
+      <p>Does your sighting involve an Ash tree?</p>
+      <div>
+        <input
+          type="radio"
+          id="ashTreeYes"
+          name="ashTree"
+          value="true"
+          checked={formData.isAshTree === true}
+          onChange={handleAshTreeChange}
+        />
+        <label htmlFor="ashTreeYes">Yes</label>
+
+        <input
+          type="radio"
+          id="ashTreeNo"
+          name="ashTree"
+          value="false"
+          checked={formData.isAshTree === false}
+          onChange={handleAshTreeChange}
+        />
+        <label htmlFor="ashTreeNo">No</label>
+
+        <input
+          type="radio"
+          id="ashTreeNotSure"
+          name="ashTree"
+          value="notSure"
+          checked={formData.isAshTree === undefined}
+          onChange={handleAshTreeChange}
+        />
+        <label htmlFor="ashTreeNotSure">Not Sure</label>
+      </div>
       <button onClick={previous}>Previous</button>
       <button onClick={next}>Next</button>
     </div>
