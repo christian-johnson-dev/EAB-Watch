54085a53aa7a1bf3deba18156a88c0964ad5796b
Christian Johnson <charcoaldust@mac.com>
2023-06-08 10:57:04 -0700

    feat: Add "secondary indicators" input to Step7 component
    This commit adds three new checkboxes to the Step7 component for secondary indicators of an emerald ash borer infestation: "Canopy Dieback", "Epicormic Shoots", and "Woodpecker Damage". The checkboxes are implemented using the `formData` state and the `handleCheckboxChange` function.

--- client/src/components/wizzardForm/Step7.js
+++ client/src/components/wizzardForm/Step7.js
@@ -1,10 +1,41 @@
 import React from "react";
 
-const Step7 = ({ previous, next }) => {
+const Step7 = ({ formData, handleCheckboxChange, previous, next }) => {
   // Component Logic
   return (
     <div>
-      <h2>Step7 reporting for duty</h2>
+      <h2>Secondary Indicators</h2>
+
+      <div>
+        <input
+          type="checkbox"
+          name="canopyDieback"
+          checked={formData.canopyDieback}
+          onChange={handleCheckboxChange}
+        />
+        <label htmlFor="canopyDieback">Canopy Dieback</label>
+      </div>
+
+      <div>
+        <input
+          type="checkbox"
+          name="epicormicShoots"
+          checked={formData.epicormicShoots}
+          onChange={handleCheckboxChange}
+        />
+        <label htmlFor="epicormicShoots">Epicormic Shoots</label>
+      </div>
+
+      <div>
+        <input
+          type="checkbox"
+          name="woodpecker"
+          checked={formData.woodpecker}
+          onChange={handleCheckboxChange}
+        />
+        <label htmlFor="woodpecker">Woodpecker</label>
+      </div>
+
       <button onClick={previous}>Previous</button>
       <button onClick={next}>Next</button>
     </div>
