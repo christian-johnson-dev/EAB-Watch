dabdefd076a5101c594ae0308c1e74983dd6fb4d
Christian Johnson <charcoaldust@mac.com>
2023-06-08 11:04:46 -0700

    feat: Add image upload functionality to Step10 component
    This commit adds image upload functionality to the Step10 component using the `handleImageUpload` and `handleImageChange` functions. The `handleImageUpload` function is used to get the selected image files from the user and pass them to the `handleImageChange` function. The `handleImageChange` function is used to update the `formData` state with the selected images.
    
    The selected images are displayed as thumbnails using the `selectedImages` state and the `map` function. The `input` element is used to allow users to select multiple image files.

--- client/src/components/wizzardForm/Step10.js
+++ client/src/components/wizzardForm/Step10.js
@@ -1,10 +1,35 @@
 import React from "react";
 
-const Step10 = ({ previous, next }) => {
+const Step10 = ({ previous, next, handleImageChange, formData }) => {
+  const { images } = formData;
+  const selectedImages = images.map((image) => URL.createObjectURL(image));
+
+  const handleImageUpload = (event) => {
+    const files = Array.from(event.target.files);
+    handleImageChange(files);
+  };
   // Component Logic
   return (
     <div>
-      <h2>Step10 reporting for duty</h2>
+      <h2>Images upload</h2>
+      {/* Thumbnails */}
+      <div className="thumbnails">
+        {selectedImages.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt="thumbnail"
+            style={{ width: "100px" }}
+          />
+        ))}
+      </div>
+      <input
+        type="file"
+        name="images"
+        multiple
+        onChange={handleImageUpload}
+      />
+
       <button onClick={previous}>Previous</button>
       <button onClick={next}>Next</button>
     </div>
