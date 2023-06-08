5f19cf8012552ab637cb7fdd6df5e3aba486f6dc
Christian Johnson <charcoaldust@mac.com>
2023-06-08 10:52:19 -0700

    feat: Add manual location entry to Step5 component
    This commit adds a new textarea input to the Step5 component that allows users to manually enter a street address and city or intersection and city for their sighting location. The `handleChange` function is used to update the `formData` state with the entered location.

--- client/src/components/wizzardForm/Step5.js
+++ client/src/components/wizzardForm/Step5.js
@@ -1,10 +1,17 @@
 import React from "react";
 
-const Step5 = ({ previous, next }) => {
+const Step5 = ({ formData, handleChange, previous, next }) => {
   // Component Logic
   return (
     <div>
-      <h2>Step5 reporting for duty</h2>
+      <h2>Location: Manual entry</h2>
+      <textarea
+        name="location"
+        placeholder="Enter a street address and city or intersection and city"
+        value={formData.location}
+        onChange={handleChange}
+      />
+
       <button onClick={previous}>Previous</button>
       <button onClick={next}>Next</button>
     </div>
