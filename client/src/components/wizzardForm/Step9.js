c7d2856174ffcfd64a572fc154531c0b56cddc12
Christian Johnson <charcoaldust@mac.com>
2023-06-08 11:00:40 -0700

    feat: Add comments and specimen checkbox to Step9 component
    This commit adds a new textarea input to the Step9 component for users to leave comments about their sighting. The `handleChange` function is used to update the `formData` state with the entered comments.
    
    This commit also adds a new checkbox to the Step9 component asking users if they have a specimen. The checkbox is implemented using the `formData` state and the `handleCheckboxChange` function.

--- client/src/components/wizzardForm/Step9.js
+++ client/src/components/wizzardForm/Step9.js
@@ -1,10 +1,30 @@
 import React from "react";
 
-const Step9 = ({ previous, next }) => {
+const Step9 = ({
+  formData,
+  handleCheckboxChange,
+  handleChange,
+  previous,
+  next,
+}) => {
   // Component Logic
   return (
     <div>
-      <h2>Step9 reporting for duty</h2>
+      <h2>Comments:</h2>
+      <textarea
+        name="comments"
+        placeholder="Feel free to describe, comment or ask a question."
+        value={formData.comments}
+        onChange={handleChange}
+      />
+      {/* Checkbox for hasSpecimen */}
+      <label htmlFor="hasSpecimen">Do you have a specimen?</label>
+      <input
+        type="checkbox"
+        name="hasSpecimen"
+        checked={formData.hasSpecimen}
+        onChange={handleCheckboxChange}
+      />
       <button onClick={previous}>Previous</button>
       <button onClick={next}>Next</button>
     </div>
