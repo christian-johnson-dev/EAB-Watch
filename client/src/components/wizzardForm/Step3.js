a4fcb30c2c4b16831b4eabba6de85f189625efb1
Christian Johnson <charcoaldust@mac.com>
2023-06-08 10:44:52 -0700

    feat: Add date input to Step3 component
    This commit adds a new date input to the Step3 component in the form. The date input allows users to select the date of their sighting. The `handleChange` function is used to update the `formData` state with the selected date.

--- client/src/components/wizzardForm/Step3.js
+++ client/src/components/wizzardForm/Step3.js
@@ -1,10 +1,18 @@
 import React from "react";
 
-const Step3 = ({ previous, next }) => {
+const Step3 = ({ formData, handleChange, previous, next }) => {
   // Component Logic
   return (
     <div>
-      <h2>Step3 reporting for duty</h2>
+      <h2>When did you make this sighting?</h2>
+      <input
+        type="date"
+        name="date"
+        placeholder="Date"
+        value={formData.date}
+        onChange={handleChange}
+      />
+
       <button onClick={previous}>Previous</button>
       <button onClick={next}>Next</button>
     </div>
