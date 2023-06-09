030b193e08e873476c5ba376df7a6f24e87ef867
Christian Johnson <charcoaldust@mac.com>
2023-06-09 16:48:57 -0700

    redesigned:  Rebuilt the map component of the sighting wizzardForm
    -- Uses Geolocation on loading to center the map on the user's location
    -- Uses the Google Maps API to display a map
    -- Allows the user to drag the pin to a specific location which updates the latitude and longitude values of the form
    -- If they return to the map, the pin will be in the same location as when they left rather than reusing geolocation

--- client/src/components/wizzardForm/Step4.js
+++ client/src/components/wizzardForm/Step4.js
@@ -1,105 +1,79 @@
 import React from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
 
-const libraries = ["places"];
-const mapContainerStyle = {
-  width: "100%",
-  height: "400px",
-};
-
-const Step4 = ({ formData, setFormData, previous, next, setCurrentStep }) => {
+const Step4 = ({ formData, setFormData, next, previous }) => {
   const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-    libraries,
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
   });
 
-  const [center, setCenter] = React.useState(null);
-
-  React.useEffect(() => {
-    console.log("isLoaded:", isLoaded);
-    console.log("loadError:", loadError);
-    console.log("center:", center);
-  }, [isLoaded, loadError, center]);
-
-  React.useEffect(() => {
-    console.log(
-      "Latitude and Longitude on page load: ",
-      formData.latitude + ", " + formData.longitude,
-    );
-    const storedLat = localStorage.getItem("latitude");
-    const storedLng = localStorage.getItem("longitude");
-
-    if (storedLat && storedLng) {
-      setCenter({ lat: parseFloat(storedLat), lng: parseFloat(storedLng) });
-    } else {
-      navigator.geolocation.getCurrentPosition((position) => {
-        setCenter({
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        });
-      });
-    }
-  }, []);
-
-  React.useEffect(() => {
-    if (center) {
-      console.log(
-        "Latitude and Longitude on map move: ",
-        center.lat + ", " + center.lng,
-      );
-      localStorage.setItem("latitude", center.lat);
-      localStorage.setItem("longitude", center.lng);
-    }
-  }, [center]);
+  const mapStyles = {
+    height: "50vh",
+    width: "100%",
+  };
 
   const handleMarkerDragEnd = (event) => {
-    console.log(
-      "Latitude and Longitude on marker drag end: ",
-      event.latLng.lat() + ", " + event.latLng.lng(),
-    );
-    setCenter({
-      lat: event.latLng.lat(),
-      lng: event.latLng.lng(),
-    });
+    const lat = event.latLng.lat();
+    const lng = event.latLng.lng();
 
     setFormData({
       ...formData,
-      latitude: event.latLng.lat(),
-      longitude: event.latLng.lng(),
+      latitude: lat,
+      longitude: lng,
     });
   };
 
+  React.useEffect(() => {
+    if (!formData.latitude && !formData.longitude) {
+      // Check if latitude and longitude are not already set
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const lat = position.coords.latitude;
+            const lng = position.coords.longitude;
+
+            setFormData({
+              ...formData,
+              latitude: lat,
+              longitude: lng,
+            });
+          },
+          (error) => {
+            console.error("Error getting current position:", error);
+          },
+        );
+      } else {
+        console.error("Geolocation is not supported by this browser.");
+      }
+    }
+  }, [formData, setFormData]);
+
   if (loadError) return "Error loading maps";
   if (!isLoaded) return "Loading Maps";
-
-  const goToSixthStep = () => {
-    setCurrentStep(6);
-  };
-
   return (
     <div>
-      {center && (
-        <GoogleMap
-          id="map"
-          mapContainerStyle={mapContainerStyle}
-          zoom={17}
-          center={center}
-        >
-          {center && (
-            <Marker
-              position={center}
-              draggable={true}
-              onDragEnd={handleMarkerDragEnd}
-            />
-          )}
-        </GoogleMap>
-      )}
-
-      <button onClick={goToSixthStep}>Use Drop Pin</button>
-      <p onClick={next}>I'll enter the location manually</p>
-
-      <button onClick={previous}>Previous</button>
+      <h2>Where did you make this sighting?</h2>
+      <GoogleMap
+        zoom={17}
+        center={{
+          lat: formData.latitude || 45.53077016741708,
+          lng: formData.longitude || -122.85460515594484,
+        }}
+        mapContainerStyle={mapStyles}
+      >
+        <MarkerF
+          position={{
+            lat: formData.latitude || 45.53077016741708,
+            lng: formData.longitude || -122.85460515594484,
+          }}
+          draggable={true}
+          onDragEnd={handleMarkerDragEnd}
+        />
+      </GoogleMap>
+      {/* Display coordinates */}
+      <p>Latitude: {formData.latitude}</p>
+      <p>Longitude: {formData.longitude}</p>
       <button onClick={next}>Next</button>
+      <button onClick={previous}>Previous</button>
     </div>
   );
 };
