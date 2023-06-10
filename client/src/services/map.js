a289b9bb0bfc048a88c9a7aa014f6fc00129e15e
Christian Johnson <charcoaldust@mac.com>
2023-06-10 16:00:41 -0700

    Created service files for interacting with the backend

---
+++ client/src/services/map.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const map = () => {
+  // Component Logic
+  return (
+    <div>
+      <h2>map reporting for duty</h2>
+      {/* Component Content */}
+    </div>
+  );
+};
+
+export default map;
---
+++ client/src/services/report.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const report = () => {
+  // Component Logic
+  return (
+    <div>
+      <h2>report reporting for duty</h2>
+      {/* Component Content */}
+    </div>
+  );
+};
+
+export default report;
