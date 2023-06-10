db35a6c6e1b121d1a5ed2438c8b9bf56925065a1
Christian Johnson <charcoaldust@mac.com>
2023-06-10 15:22:31 -0700

    Add navigation components to application
    This commit adds placeholder reaact components NavItem and NavBar to the application

---
+++ client/src/components/Navigation/NavItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavItem = () => {
+  // Component Logic
+  return (
+    <div>
+      <h2>NavItem reporting for duty</h2>
+      {/* Component Content */}
+    </div>
+  );
+};
+
+export default NavItem;
---
+++ client/src/components/Navigation/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  // Component Logic
+  return (
+    <div>
+      <h2>Navbar reporting for duty</h2>
+      {/* Component Content */}
+    </div>
+  );
+};
+
+export default Navbar;
