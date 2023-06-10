220e2acc1d40c86deb101f85ff67dff5a607d460
Christian Johnson <charcoaldust@mac.com>
2023-06-10 15:44:47 -0700

    Added placeholder content

---
+++ client/src/components/Pages/EABInOregon/HistoryPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HistoryPage = () => {
+  // Component Logic
+  return (
+    <div>
+      <h2>HistoryPage reporting for duty</h2>
+      {/* Component Content */}
+    </div>
+  );
+};
+
+export default HistoryPage;
