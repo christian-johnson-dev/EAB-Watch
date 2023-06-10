32b73864ac7983a6385ee571c2cf1b7a7ee459a2
Christian Johnson <charcoaldust@mac.com>
2023-06-10 11:42:26 -0700

    Remove unnecessary backend message retrieval
        - Removed the code that fetched a message from the backend API
        - Updated the component structure to remove the display of the backend message
    
    The backend message retrieval and display were unnecessary for the functionality of the component. Removing this code simplifies the codebase and improves performance.

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,9 @@ import axios from "axios";
 import MultiStepForm from "./components/wizzardForm/MultiStepForm";
 
 const App = () => {
-  const [message, setMessage] = React.useState("Loading...");
-  React.useEffect(() => {
-    axios
-      .get("http://localhost:8000/api")
-      .then((res) => setMessage(res.data.message))
-      .catch((err) => console.log(err));
-  }, []);
-
   return (
     <div className="App">
       <h1>React App</h1>
-      <h2>Message from backend: {message} </h2>
       <MultiStepForm />
     </div>
   );
