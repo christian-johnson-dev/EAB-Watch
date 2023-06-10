f62ab9c870cfcd533abffba80dec92d77c1337c1
Christian Johnson <charcoaldust@mac.com>
2023-06-10 15:31:44 -0700

    Created page components
    Created placeholer reaact components for the following pages:
    - AboutPage
    - EABInfoPage
    - LearnToReportPage
    - ReportPage
    - ReportedSightingsPage
    - WelcomePage

---
+++ client/src/components/Pages/ReportPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ReportPage = () => {
+  // Component Logic
+  return (
+    <div>
+      <h2>ReportPage reporting for duty</h2>
+      {/* Component Content */}
+    </div>
+  );
+};
+
+export default ReportPage;
---
+++ client/src/components/Pages/ReportedSightingsPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ReportedSightingsPage = () => {
+  // Component Logic
+  return (
+    <div>
+      <h2>ReportedSightingsPage reporting for duty</h2>
+      {/* Component Content */}
+    </div>
+  );
+};
+
+export default ReportedSightingsPage;
