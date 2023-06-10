47cd10f4dae344ba9607cfa7ea3cf2d14acd28d3
Christian Johnson <charcoaldust@mac.com>
2023-06-10 15:41:38 -0700

    Created subpages for EAB in Oregon and Learn to Report
    Created placeholder react components for subpages of EAB in Oregon and Learn to Report
      - EAB in Oregon
        - History
        - Consequences for Oregon
        - What You Can Do
        - FAQ
      - Learn to Report
        - Ash Identification
        - Primary Indicators
        - Secondary Indicators

---
+++ client/src/components/Pages/LearnToReport/AshIdentificationPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AshIdentificationPage = () => {
+  // Component Logic
+  return (
+    <div>
+      <h2>AshIdentificationPage reporting for duty</h2>
+      {/* Component Content */}
+    </div>
+  );
+};
+
+export default AshIdentificationPage;
---
+++ client/src/components/Pages/LearnToReport/SecondaryIndicatorsPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SecondaryIndicatorsPage = () => {
+  // Component Logic
+  return (
+    <div>
+      <h2>SecondaryIndicatorsPage reporting for duty</h2>
+      {/* Component Content */}
+    </div>
+  );
+};
+
+export default SecondaryIndicatorsPage;
