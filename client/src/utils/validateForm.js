393c68c0744d571d619ffac786a73ff0ca4b1f69
Christian Johnson <charcoaldust@mac.com>
2023-06-10 16:02:40 -0700

    Placeholder for Form Validation Utility

---
+++ client/src/utils/validateForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const validateForm = () => {
+  // Component Logic
+  return (
+    <div>
+      <h2>validateForm reporting for duty</h2>
+      {/* Component Content */}
+    </div>
+  );
+};
+
+export default validateForm;
