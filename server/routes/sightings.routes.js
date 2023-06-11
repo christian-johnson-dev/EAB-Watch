f91f4d94d4a016ef8e0b3f30ee2ba133a9da0085
Christian Johnson <charcoaldust@mac.com>
2023-06-11 12:38:58 -0700

    Added multer to routes

--- server/routes/sightings.routes.js
+++ server/routes/sightings.routes.js
@@ -1,5 +1,12 @@
 const SightingsController = require("../controllers/sightings.controller");
+
+const multer = require("multer"); // import multer middleware
+const upload = multer({ dest: "uploads/" }); // set up the destination folder for uploaded files
 module.exports = (app) => {
   app.get("/api", SightingsController.index);
-  app.post("/api/sighting", SightingsController.createSighting);
+  app.post(
+    "/api/sighting",
+    upload.array("images", 12),
+    SightingsController.createSighting,
+  );
 };
