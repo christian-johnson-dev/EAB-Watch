09a3282ba5c2ecc913500b20b409a634d0a76357
Christian Johnson <charcoaldust@mac.com>
2023-06-11 16:48:54 -0700

    Add thumbnail image removal

--- client/src/components/Form/Step10.js
+++ client/src/components/Form/Step10.js
@@ -1,6 +1,12 @@
 import React from "react";
 
-const Step10 = ({ previous, next, handleImageChange, formData }) => {
+const Step10 = ({
+  previous,
+  next,
+  handleImageChange,
+  setFormData,
+  formData,
+}) => {
   const { images } = formData;
   const selectedImages = images.map((image) => URL.createObjectURL(image));
 
@@ -8,25 +14,57 @@ const Step10 = ({ previous, next, handleImageChange, formData }) => {
     const files = Array.from(event.target.files);
     handleImageChange(files);
   };
-  // Component Logic
+
+  const handleRemoveImage = (indexToRemove) => {
+    setFormData({
+      ...formData,
+      images: formData.images.filter((_, index) => index !== indexToRemove),
+    });
+  };
+
   return (
     <div>
       <h2>Images upload</h2>
       {/* Thumbnails */}
       <div className="thumbnails">
         {selectedImages.map((image, index) => (
-          <img
+          <div
             key={index}
-            src={image}
-            alt="thumbnail"
-            style={{ width: "100px" }}
-          />
+            style={{ display: "inline-block", position: "relative" }}
+          >
+            <img
+              src={image}
+              alt="thumbnail"
+              style={{ width: "100px" }}
+            />
+            <div
+              style={{
+                position: "absolute",
+                right: 0,
+                top: 0,
+                cursor: "pointer",
+                background: "red",
+                color: "white",
+                borderRadius: "50%",
+                width: "20px",
+                height: "20px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                rotate: "45deg",
+              }}
+              onClick={() => handleRemoveImage(index)}
+            >
+              +
+            </div>
+          </div>
         ))}
       </div>
       <input
         type="file"
         name="images"
         multiple
+        accept="image/jpeg, image/png, image/webp"
         onChange={handleImageUpload}
       />
 
