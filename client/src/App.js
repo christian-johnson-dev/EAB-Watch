28623efb938baf54246a0be5405838ccbab4f94e
Christian Johnson <charcoaldust@mac.com>
2023-06-12 14:08:29 -0700

    Added app routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,47 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import axios from "axios";
-import MultiStepForm from "./components/Form/MultiStepForm";
+import WelcomePage from "./components/Pages/WelcomePage";
+import AboutPage from "./components/Pages/AboutPage";
+import EABInfoPage from "./components/Pages/EABInfoPage";
+import LearnToReportPage from "./components/Pages/LearnToReportPage";
+import ReportPage from "./components/Pages/ReportPage";
+import SightingsPage from "./components/Pages/SightingsPage";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>React App</h1>
-      <MultiStepForm />
-    </div>
+    <Router>
+      <div className="App">
+        <h1>React App</h1>
+
+        <Routes>
+          <Route
+            path="/"
+            element={<WelcomePage />}
+          />
+          <Route
+            path="/about"
+            element={<AboutPage />}
+          />
+          <Route
+            path="/eab-info"
+            element={<EABInfoPage />}
+          />
+          <Route
+            path="/learn-to-report"
+            element={<LearnToReportPage />}
+          />
+          <Route
+            path="/report"
+            element={<ReportPage />}
+          />
+          <Route
+            path="/sightings"
+            element={<SightingsPage />}
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
