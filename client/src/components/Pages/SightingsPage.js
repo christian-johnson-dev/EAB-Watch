d76074d2e3739410a7778f3fa3f447cf8b983ace
Christian Johnson <charcoaldust@mac.com>
2023-06-12 14:09:05 -0700

    Renamed to SightingsPage

---
+++ client/src/components/Pages/SightingsPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SightingsPage = () => {
+  // Component Logic
+  return (
+    <div>
+      <h2>SightingsPage reporting for duty</h2>
+      {/* Component Content */}
+    </div>
+  );
+};
+
+export default SightingsPage;
