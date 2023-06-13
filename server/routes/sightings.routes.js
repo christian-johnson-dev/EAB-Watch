37199ee59d7de1f0d1f3bdce29b38dbcb90912f4
Christian Johnson <charcoaldust@mac.com>
2023-06-13 14:32:23 -0700

    Added Read Update and Delete routes

--- server/routes/sightings.routes.js
+++ server/routes/sightings.routes.js
@@ -4,6 +4,9 @@ const multer = require("multer"); // import multer middleware
 const upload = multer({ dest: "uploads/" }); // set up the destination folder for uploaded files
 module.exports = (app) => {
   app.get("/api", SightingsController.index);
+  app.get("/api/sightings", SightingsController.getAllSightings);
+  app.put("/api/sighting/:id", SightingsController.updateSighting);
+  app.delete("/api/sighting/:id", SightingsController.deleteSighting);
   app.post(
     "/api/sighting",
     upload.array("images", 12),
