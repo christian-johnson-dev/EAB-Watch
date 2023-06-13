f1c180469e58e4931dad638ddf5a506472cc6f50
Christian Johnson <charcoaldust@mac.com>
2023-06-13 15:53:57 -0700

    Created NavItem

--- client/src/components/Navigation/NavItem.js
+++ client/src/components/Navigation/NavItem.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const NavItem = () => {
+const NavItem = ({ to, children }) => {
   // Component Logic
   return (
-    <div>
-      <h2>NavItem reporting for duty</h2>
-      {/* Component Content */}
+    <div className="mr-4">
+      <Link
+        to={to}
+        className="text-lg no-underline text-grey-darkest hover:text-blue-dark font-sans font-bold"
+      >
+        {children}
+      </Link>
     </div>
   );
 };
