9796a15347073fd28b014a110c499135428839f0
Christian Johnson <charcoaldust@mac.com>
2023-06-19 07:11:55 -0700

    updated breakpoints, created project named colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,39 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "489px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        dark: "black",
+        light: "white",
+        verified: "#1e8b26",
+        pending: "#b04200",
+        reclassified: "#505152",
+        eab: "#2ba696",
+      },
+      fontFamily: {
+        baloo: ["'Baloo Paaji 2'"],
+        sans: ["Helvetica, Helvetica Neue"],
+      },
+      letterSpacing: {
+        widest: ".3em",
+      },
+      transformOrigin: {
+        0: "0%",
+        50: "50%",
+        100: "100%",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      transformOrigin: ["hover", "focus", "responsive"],
+    },
   },
   plugins: [],
 };
