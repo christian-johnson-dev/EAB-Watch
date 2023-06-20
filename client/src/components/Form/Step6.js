59bf1059e71818f2ff62397463b86a61f38cec8b
Christian Johnson <charcoaldust@mac.com>
2023-06-20 14:37:54 -0700

    Styling for radio buttons

--- client/src/components/Form/Step6.js
+++ client/src/components/Form/Step6.js
@@ -50,36 +50,40 @@ const Step6 = ({
           </p>
         </div>
         <p>Does your sighting involve an Ash tree?</p>
-        <div>
-          <input
-            type="radio"
-            id="ashTreeYes"
-            name="ashTree"
-            value="true"
-            checked={formData.isAshTree === true}
-            onChange={handleAshTreeChange}
-          />
-          <label htmlFor="ashTreeYes">Yes</label>
-
-          <input
-            type="radio"
-            id="ashTreeNo"
-            name="ashTree"
-            value="false"
-            checked={formData.isAshTree === false}
-            onChange={handleAshTreeChange}
-          />
-          <label htmlFor="ashTreeNo">No</label>
-
-          <input
-            type="radio"
-            id="ashTreeNotSure"
-            name="ashTree"
-            value="notSure"
-            checked={formData.isAshTree === undefined}
-            onChange={handleAshTreeChange}
-          />
-          <label htmlFor="ashTreeNotSure">Not Sure</label>
+        <div className="border-2  mx-auto rounded-lg border-dark flex gap-5 justify-center max-w-max px-6 py-3 mt-2">
+          <div className="flex gap-1">
+            <input
+              type="radio"
+              id="ashTreeYes"
+              name="ashTree"
+              value="true"
+              checked={formData.isAshTree === true}
+              onChange={handleAshTreeChange}
+            />
+            <label htmlFor="ashTreeYes">Yes</label>
+          </div>
+          <div className="flex gap-1">
+            <input
+              type="radio"
+              id="ashTreeNo"
+              name="ashTree"
+              value="false"
+              checked={formData.isAshTree === false}
+              onChange={handleAshTreeChange}
+            />
+            <label htmlFor="ashTreeNo">No</label>
+          </div>
+          <div className="flex gap-1">
+            <input
+              type="radio"
+              id="ashTreeNotSure"
+              name="ashTree"
+              value="notSure"
+              checked={formData.isAshTree === undefined}
+              onChange={handleAshTreeChange}
+            />
+            <label htmlFor="ashTreeNotSure">Not Sure</label>
+          </div>
         </div>
         <FormNavButtons
           previous={previous}
