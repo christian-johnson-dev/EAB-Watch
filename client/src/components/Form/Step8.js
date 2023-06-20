194228e026e197695bc58ccc0feeffd1ce696ef0
Christian Johnson <charcoaldust@mac.com>
2023-06-20 00:36:19 -0700

    Added cancel link

--- client/src/components/Form/Step8.js
+++ client/src/components/Form/Step8.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormNavButtons from "../Shared/FormNavButtons";
+import { Link } from "react-router-dom";
 
 const Step8 = ({
   formData,
@@ -14,6 +15,12 @@ const Step8 = ({
   return (
     <div>
       <div className="card">
+        <Link
+          to="/sightings"
+          className="card-cancel"
+        >
+          Cancel
+        </Link>
         <div className="heading-container">
           <p className="step-heading">Section 5 of 6:</p>
           <h3 className="form-heading">
